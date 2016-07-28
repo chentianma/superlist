a333a493e25171bdbcc24540da94b71c3c979aa8
chentianma <chentianma@github.com>
2016-07-28 11:35:28 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -11,15 +12,41 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+        print('Finish the test!')
 
     def test_start_a_list_and_retrieve_it(self):
 
         # 去看应用首页
         self.browser.get('http://localhost:8000')
+
         # 检查网页标题内是否有“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        head_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', head_text)
+
         # 应用邀请他输入一个待办事项.....
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 在文本框里输入“买一台Macbook Pro”
+        inputbox.send_keys('买一台Macbook Pro')
+
+        # 点击回车确定，页面刷新
+        # 待办事项表格中显示了“1.买一台Macbook Pro”
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '买一台Macbook Pro' for row in rows)
+        )
+
+        # 页面中又显示了一个文本框，可以输入其他的待办事项
+        # 输入其他待办事项
+        # 结束
 
 
 if __name__ == '__main__':
