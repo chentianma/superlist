5fb8bd658eb207c5d658b7d0e4692b651d31ac8b
chentianma <chentianma@github.com>
2016-08-01 15:40:11 +0800

    Test: can the blank save what I sent

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,12 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
         print('Finish the test!')
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows],
+                      'New To-Do list did not appear in table, the text was:\n%s' % row_text)
+
     def test_start_a_list_and_retrieve_it(self):
 
         # 去看应用首页
@@ -37,11 +43,14 @@ class NewVisitorTest(unittest.TestCase):
         # 点击回车确定，页面刷新
         # 待办事项表格中显示了“1.买一台Macbook Pro”
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1.买一台Macbook Pro')
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('2.一个苹果的故事')
+        inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('买一台Macbook Pro', [row.text for row in rows])
+        self.check_for_row_in_list_table('1.买一台Macbook Pro')
+        self.check_for_row_in_list_table('2.一个苹果的故事')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 输入其他待办事项
